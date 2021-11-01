f542e8ba5ff8405e104acc5bcf8cd1c1770e37f9
Olga <o2-2010@mail.ru>
2021-11-01 08:41:32 +0300

    Spring6 - mvc. (пока без rest и покрытия тестами)

---
+++ spring-mvc/adressbook/src/test/kotlin/org/spring/mvc/adressbook/AdressbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.spring.mvc.adressbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdressbookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
